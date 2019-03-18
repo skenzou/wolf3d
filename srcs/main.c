2187c06424961e762c9bf4e61b999733b33dbd87
Mohamed Idrissi <midrissi@w-r6-p1.s19.be>
2019-03-18 12:52:37 +0100

    print map

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: midrissi <midrissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 09:45:23 by midrissi          #+#    #+#             */
-/*   Updated: 2019/03/18 11:57:12 by midrissi         ###   ########.fr       */
+/*   Updated: 2019/03/18 12:52:16 by midrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,20 @@ t_wolf3d		*init_wolf3d(int fd)
 	return (w);
 }
 
+void			print_map(t_wolf3d *w)
+{
+	int y;
+	int x;
+
+	y = 1;
+	while (++y < w->map->h && (x = -1))
+	{
+		while (++x < w->map->w)
+			ft_printf("%2d", w->map->board[y][x]);
+		ft_printf("\n");
+	}
+}
+
 int				main(int argc, char **argv)
 {
 	t_wolf3d	*w;
@@ -43,5 +57,6 @@ int				main(int argc, char **argv)
 		return (0);
 	}
 	w = init_wolf3d(fd);
+	print_map(w);
 	return (0);
 }
